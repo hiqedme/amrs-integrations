22ae9fd2b567fd20d168ed62196eaafcc9fedbaf
derrick rono <derrickrono@gmail.com>
2022-03-15 15:12:06 +0300

    fix http client and add sms sending logic

--- packages/sms-reminders/app/appointments.ts
+++ packages/sms-reminders/app/appointments.ts
@@ -2,6 +2,7 @@ import moment from "moment";
 import { Consumer, Message, Producer } from "redis-smq";
 import { Patient } from "../models/patient";
 import { dailyAppointmentsquery } from "../models/queries";
+import { SendSMS } from "./sms";
 export async function RetrieveAppointments(
   daysBeforeRTC: string,
   location: string,
@@ -11,43 +12,63 @@ export async function RetrieveAppointments(
     .add(daysBeforeRTC, "days")
     .format("YYYY-MM-DD");
   let appointments = await dailyAppointmentsquery(appointmentDate);
-  let queueResponse = await QueueAppointments(appointments);
-
-  return queueResponse;
+  await QueueAppointments(appointments);
 }
 
 export async function QueueAppointments(patients: Patient[]) {
+  let count = 0;
+  const producer = new Producer();
   patients.forEach((element) => {
     const message = new Message();
     message
       .setBody(JSON.stringify(element))
       .setTTL(3600000) // in millis
       .setQueue("appointments_queue");
-    const producer = new Producer();
+   
     producer.produce(message, (err) => {
       if (err) console.log(err);
       else {
         const msgId = message.getId(); // string
         console.log("Successfully produced. Message ID is ", msgId);
+        count++;
       }
     });
   });
+  // if all appointments were queued successfuly, shutdown producer and exit with code 0
+  if (count === patients.length) {
+    producer.shutdown()
+    process.exit(0);
+  }
 }
 
 export async function SendNotifications() {
   const consumer = new Consumer();
 
-const messageHandler = (msg: { getBody: () => any; }, cb: () => void) => {
-   const payload = msg.getBody();
-   console.log('Message payload', payload);
-   cb(); // acknowledging the message
-};
+  const messageHandler = async (
+    msg: { getBody: () => any },
+    cb: () => void
+  ) => {
+    const payload: Patient = msg.getBody();
+    console.log("Message payload", payload);
+    // send sms
+    let response = await SendSMS(payload);
+    //check response for success or error. if error
+    cb(); // acknowledging the message
+  };
 
-consumer.consume('appointments_queue', false, messageHandler, (err, isRunning) => {
-   if (err) console.error(err);
-   // the message handler will be started only if the consumer is running
-   else console.log(`Message handler has been registered. Running status: ${isRunning}`); // isRunning === false
-});
+  consumer.consume(
+    "appointments_queue",
+    false,
+    messageHandler,
+    (err, isRunning) => {
+      if (err) console.error(err);
+      // the message handler will be started only if the consumer is running
+      else
+        console.log(
+          `Message handler has been registered. Running status: ${isRunning}`
+        ); // isRunning === false
+    }
+  );
 
-consumer.run();
+  consumer.run();
 }
---
+++ packages/sms-reminders/app/sms.ts
@@ -0,0 +1,34 @@
+import config from "@amrs-integrations/core";
+import qs from "qs";
+import { Patient } from "../models/patient";
+import moment from "moment";
+import { AfricasTalkingResponse } from "../models/sms";
+
+export async function SendSMS(params: any) {
+  let smsParams:Patient = JSON.parse(params)
+  let msisdn = parseInt(smsParams.phone_number, 10);
+  // TODO: Check the telco used for the provider then pick approapriate shortcode
+  let from = '';
+  let appointmentDate = moment(smsParams.rtc_date).format("YYYY-MM-DD");
+  let sms =
+    "Booked: Please come for your next appointment on " + appointmentDate;
+  let httpClient = new config.HTTPInterceptor(
+    config.sms.url || "",
+    "",
+    "",
+    "sms"
+  );
+  let sendSMSResponse: AfricasTalkingResponse = await httpClient.axios(
+    "/messaging",
+    {
+      method: "post",
+      data: qs.stringify({
+        username: "sandbox",
+        to: "254" + msisdn,
+        message: sms,
+        from:from
+      }),
+    }
+  );
+  return sendSMSResponse;
+}
