93ba3950c663ac1670cba8830f625dd832d4e3dd
jacinta254 <jecihjoy@gmail.com>
2022-05-10 15:36:49 +0300

    Metadata definitions and patient creation logic

---
+++ packages/patient-registry/app/ClientRegistryLookupDictionaries/gender.ts
@@ -0,0 +1,22 @@
+const Gender = [
+  {
+    amrs: "M",
+    value: "male",
+    label: "Male",
+  },
+  {
+    amrs: "F",
+    value: "female",
+    label: "Female",
+  },
+  {
+    value: "unspecified",
+    label: "Unspecified",
+  },
+  {
+    value: "intersex",
+    label: "Intersex",
+  },
+];
+
+export default Gender;
