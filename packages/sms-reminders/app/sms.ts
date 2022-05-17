f6376b544dfe59aaa1b70a0a758b2401a06ee67c
derrick rono <derrickrono@gmail.com>
2022-05-17 13:25:36 +0300

    add approved message templates and types

--- packages/sms-reminders/app/sms.ts
+++ packages/sms-reminders/app/sms.ts
@@ -1,4 +1,5 @@
 import config from "@amrs-integrations/core";
+import dictionary from "../templates/sms_dictionary.json"
 import qs from "qs";
 import { Patient } from "../models/patient";
 import moment from "moment";
@@ -13,32 +14,39 @@ export async function SendSMS(params: any) {
     const phoneNumber = parsePhoneNumber(smsParams.phone_number,"KE")
   let from = '';
   let appointmentDate = moment(smsParams.rtc_date).format("YYYY-MM-DD");
-  let sms =
-    "Booked: Please come for your next appointment on " + appointmentDate;
+  let sms ="";
+  let personName = smsParams.person_name;
+  if(smsParams.language === "english"){
+    sms = dictionary.templates.find((x: { type: string; }) => x.type === smsParams.messageType)?.english || ""
+  }else{
+    sms = dictionary.templates.find((x: { type: string; }) => x.type === smsParams.messageType)?.kiswahili || ""
+  }
   let httpClient = new config.HTTPInterceptor(
     config.sms.url || "",
     "",
     "",
     "sms"
   );
-  //validate and format phone numbers appropriately
-
-  let sendSMSResponse: AfricasTalkingResponse = await httpClient.axios(
-    "/services/sendsms/",
-    {
-      method: "post",
-      data: qs.stringify({
-        shortcode: config.sms.shortCode,
-        partnerID: config.sms.partnerID,
-        apikey: config.sms.apiKey,
-        mobile: phoneNumber.number,
-        timeToSend:smsParams.timeToSend,
-        message: sms
-      }),
+  if(sms !==""){
+    let smsContent =sms.replace("$personName$",personName).replace("$rtc_date$",appointmentDate)
+    let sendSMSResponse: AfricasTalkingResponse = await httpClient.axios(
+      "/services/sendsms/",
+      {
+        method: "post",
+        data: qs.stringify({
+          shortcode: config.sms.shortCode,
+          partnerID: config.sms.partnerID,
+          apikey: config.sms.apiKey,
+          mobile: phoneNumber.number,
+          timeToSend:smsParams.timeToSend,
+          message: smsContent
+        }),
+      }
+    );
+    return sendSMSResponse;
+    }else{
+    console.log("Invalid phone number")
     }
-  );
-  return sendSMSResponse;
-  }else{
-  console.log("Invalid phone number")
   }
+  
 }
