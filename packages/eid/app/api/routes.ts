c973929f58c22507b557016a1473c8f726454a24
derrick rono <derrickrono@gmail.com>
2023-01-12 15:05:19 +0300

    POC-12 #comment add logic to resolve patient id and post data to AMRS through openhim
    Co-authored-by: Faith Kamau <hiqedme@users.noreply.github.com>

--- packages/eid/app/api/routes.ts
+++ packages/eid/app/api/routes.ts
@@ -1,11 +1,13 @@
 import { ResponseToolkit, ServerRoute } from "@hapi/hapi";
+import ExtractVLAndPostToETL from "../services/vl_extractor";
 
 export const apiRoutes: ServerRoute[] = [
   {
     method: "GET",
     path: "/api/push",
     handler: async function (request, h: ResponseToolkit) {
-     console.log(request.raw);
+      let convertionService= new ExtractVLAndPostToETL();
+      await convertionService.readCSVAndPost();
      return "success"
     },
   }
