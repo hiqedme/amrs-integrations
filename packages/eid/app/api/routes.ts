fa57434c7f4ab154fd66de83bab42ab0b41b027e
Faith Kamau <121166087+hiqedme@users.noreply.github.com>
2023-01-19 14:41:29 +0300

    POC 29: csv upload

--- packages/eid/app/api/routes.ts
+++ packages/eid/app/api/routes.ts
@@ -1,6 +1,15 @@
 import { ResponseToolkit, ServerRoute } from "@hapi/hapi";
 import ExtractVLAndPostToETL from "../services/vl_extractor";
+import UploadSaveAndArchiveCSV from "../services/csv_upload";
 
+let payload1: any = {
+  payload: {
+    output: "stream",
+    parse: true,
+    multipart: true,
+    allow: "multipart/form-data",
+  },
+};
 export const apiRoutes: ServerRoute[] = [
   {
     method: "GET",
@@ -9,6 +18,24 @@ export const apiRoutes: ServerRoute[] = [
       let convertionService= new ExtractVLAndPostToETL();
       await convertionService.readCSVAndPost();
      return "success"
+    },
+  },
+  {
+   
+    method: "POST",
+    path: "/uploads",
+    options: payload1,
+    handler: async (request: any, h) => {
+     // console.log("sisi ndio tuko");
+        // const { payload } = request;
+        // return payload;
+      const file = request.payload.file;
+      //  console.log(file);
+      let uploadService= new UploadSaveAndArchiveCSV();
+      await uploadService.uploadFile(file);
+
+      // You can now process the CSV file
+      return "CSV Uploaded";
     },
   }
 ];
--- packages/eid/app/helpers/validators.ts
+++ packages/eid/app/helpers/validators.ts
@@ -1,7 +1,9 @@
 import _ from "lodash";
-
+// import * as fast_csv from "fast-csv";
+import * as fs from "fs";
+import * as Papa from "papaparse";
 export default class Validators {
- checkStatusOfViralLoad(viralLoadPayload:string) {
+  checkStatusOfViralLoad(viralLoadPayload: string) {
     var status = 0;
     var hasNumbersOnly = /^[0-9]*(?:\.\d{1,2})?$/;
     var hasLessThanSymbol = /</g;
@@ -16,7 +18,7 @@ export default class Validators {
       status = 1;
     } else if (
       hasLessThanSymbol.test(viralLoadResult) ||
-      viralLoadPayload.trim() === 'Target Not Detected'
+      viralLoadPayload.trim() === "Target Not Detected"
     ) {
       status = 0;
     } else {
@@ -24,13 +26,49 @@ export default class Validators {
     }
     return status;
   }
-   removeWhiteSpace(param:string) {
+  removeWhiteSpace(param: string) {
     var whitePaceVar;
-    if (param === '' || param === null) {
-      whitePaceVar = '';
+    if (param === "" || param === null) {
+      whitePaceVar = "";
     } else {
-      whitePaceVar = param.replace(/\s+/g, '');
+      whitePaceVar = param.replace(/\s+/g, "");
     }
     return whitePaceVar;
+  }
+  validateCsv(file: any) {
+    // console.log(file.mimetype, file.hapi.headers['content-type']);
+    if (file.hapi.headers["content-type"] !== "text/csv") {
+      return { error: "Invalid file type. Only CSV files are allowed" };
+    }
+    if (file.size > 10000) {
+      //10MB
+      return { error: "File size too large" };
+    }
+    // const validatecols = this.validateColumns(file,['Worksheet'])
+
+    return true;
+  }
+  validateColumns(filePath: fs.PathLike, expectedColumns: any) {
+   // console.log(expectedColumns);
+    const file = fs.readFileSync(filePath, "utf-8");
+
+    return new Promise<void>((resolve, reject) => {
+      //let headers: string | any[];
+      const headers = Papa.parse(file, {
+        header: true,
+      }).meta.fields;
+      //console.log(headers);
+      let missingColumns = _.difference(expectedColumns, headers || []);
+      if (missingColumns.length > 0) {
+        reject(
+          `The following columns are missing: ${missingColumns.join(", ")}`
+        );
+      } else {
+       // console.log("all columns present");
+        resolve();
+      }
+
+      // .on('error', reject);
+    });
   }
 }
