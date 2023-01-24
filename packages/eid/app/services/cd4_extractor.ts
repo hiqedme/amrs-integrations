48f2c862d5dec80f114042c305d6762d624dbf4d
Faith Kamau <121166087+hiqedme@users.noreply.github.com>
2023-01-24 11:57:17 +0300

    accessing and extracting the CD4 count

--- packages/eid/app/services/cd4_extractor.ts
+++ packages/eid/app/services/cd4_extractor.ts
@@ -12,67 +12,73 @@ export default class ExtractCD4AndPostToETL {
         path.join(path.dirname(__dirname), "../app/uploads/cd4_file.csv"),
         "utf-8"
       );
-      console.log(file);
-      // Replace spaces in headers with underscores
+      // format header
+      const options = {
+        header: true,
+        transformHeader: (header: string) => {
+          // Replace spaces in headers with underscores
+          header = header.replace(/\s+/g, "_").toLowerCase();
+          // Replace # with 'no' in headers
+          header = header.replace("#", "no");
+          //replace /(all special characters) with _
+          header = header.replace(/[^a-zA-Z0-9]/g, "_");
+          return header;
+        },
+      };
 
-      // const options = {
-      //   header: true,
-      //   transformHeader: (header: string) =>
-      //     header.replace(/\s+/g, "_").toLowerCase(),
-      // };
-      // const rows = Papa.parse(file, options).data;
-      // let ResultData: any = [];
-      // // Iterate through rows and make POST request
-      // for (let row of rows) {
-      //   // Get patient UUID using identifier
-      //   let data: any = row;
-      //   let getPatient = new GetPatient();
-      //   let patientUUID: any = await getPatient.getPatientUUIDUsingIdentifier(
-      //     data.patient_ccc_no
-      //   );
-      //   //     console.log("PATIENT UUID"+ patientUUID );
+      const rows = Papa.parse(file, options).data;
+      console.log(rows);
+      let ResultData: any = [];
+      // Iterate through rows and make POST requests
+      for (let row of rows) {
+        // Get patient UUID using identifier
+        let data: any = row;
+        let getPatient = new GetPatient();
+        let patientUUID: any = await getPatient.getPatientUUIDUsingIdentifier(
+          data.ampath_no
+        );
 
-      //   if (patientUUID.length > 0) {
-      //     let validator = new Validators();
-      //     let valid = validator.checkStatusOfViralLoad(data.lab_viral_load);
-      //     if (valid === 0 || valid === 1) {
-      //       data.viral_load = 0;
-      //       let collection_date = moment
-      //         .utc(data.collection_date, "DD/MM/YYYY")
-      //         .add(3, "hours")
-      //         .format();
-      //       let obs: EIDPayloads.Observation = {
-      //         person: patientUUID[0].uuid,
-      //         concept: "457c741d-8f71-4829-b59d-594e0a618892",
-      //         obsDatetime: collection_date,
-      //         value: valid = 1 ? data.viral_load : 0,
-      //       };//correct concept for cd4
+        // console.log("PATIENT UUID" + patientUUID);
 
-      //       ResultData.push(obs);
-      //       // let httpClient = new config.HTTPInterceptor(
-      //       //   config.dhp.url || "http://10.50.80.56:5001/eid/csv",
-      //       //   "",
-      //       //   "",
-      //       //   "dhp",
-      //       //   ""
-      //       // );
+        if (patientUUID.length > 0) {
+          //initialize variables
+          let validator = new Validators();
+          const cd4_abs: number = data.ampath_no;
 
-      //       // httpClient.axios
-      //       //   .post("", obs)
-      //       //   .then(async (openHIMResp: any) => {
-      //       //     console.log("VL saved successfully", openHIMResp.identifier);
-      //       //   })
-      //       //   .catch((err: any) => {
-      //       //     console.log("Error", err);
-      //       //   });
-      //     } else {
-      //       console.log(data.lab_viral_load);
-      //     }
-      //   }
-      // }
+          let collection_date = moment
+            .utc(data.date_collected_drawn, "YYYY-MM-DD")
+            .add(3, "hours")
+            .format("DD/MM/YYYY");
 
-      // console.log(ResultData);
-      // return ResultData;
+          let obs: EIDPayloads.Observation = {
+            person: patientUUID[0].uuid,
+            concept: "457c741d-8f71-4829-b59d-594e0a618892",
+            obsDatetime: collection_date,
+            value: cd4_abs,
+            order: "",
+          }; //correct concept for cd4
+
+          ResultData.push(obs);
+
+          let httpClient = new config.HTTPInterceptor(
+            config.dhp.url || "http://10.50.80.56:5001/eid/csv",
+            "",
+            "",
+            "dhp",
+            ""
+          );
+          httpClient.axios
+            .post("", obs)
+            .then(async (openHIMResp: any) => {
+              console.log("CD4 saved successfully", openHIMResp.identifier);
+            })
+            .catch((err: any) => {
+              console.log("Error", err);
+            });
+        }
+      }
+      console.log(ResultData);
+      return ResultData;
       console.log("successfuly extracted");
     } catch (err) {
       console.log(err);
