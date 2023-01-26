ee6a690115a5c16ceff86030f4fff37fb6d3c135
Faith Kamau <121166087+hiqedme@users.noreply.github.com>
2023-01-26 10:53:48 +0300

    removed obscured code

--- packages/eid/app/services/cd4_extractor.ts
+++ packages/eid/app/services/cd4_extractor.ts
@@ -79,6 +79,7 @@ export default class ExtractCD4AndPostToETL {
       }
       console.log(ResultData);
       return ResultData;
+      console.log("successfuly extracted");
     } catch (err) {
       console.log(err);
     }
