c0ee5f32c8b83d2205c3bcbbd92daeab7ee1a7f4
SharleenAwinja <awinjasharleen@gamil.com>
2023-03-23 10:55:15 +0300

    create rde-sync api

--- packages/rde-sync/app/api/routes.ts
+++ packages/rde-sync/app/api/routes.ts
@@ -1,14 +1,18 @@
 import { ServerRoute } from "@hapi/hapi";
-import PatientService from "../services/patient.service";
+import PatientService, { RequestParams } from "../services/patient.service";
 
 export const apiRoutes: ServerRoute[] = [
   {
-    method: "GET",
+    method: "POST",
     path: "/api/rde-sync/queue",
     handler: async function (request, h) {
       const patientService = new PatientService();
 
-      return "";
+      await patientService.queueRDEPatients(
+        request.payload as RequestParams,
+        h
+      );
+      return "success";
     },
   },
 ];
--- packages/rde-sync/app/services/patient.service.ts
+++ packages/rde-sync/app/services/patient.service.ts
@@ -1,12 +1,57 @@
-import pool from "../db";
+import { AMRS_POOL, ETL_POOL } from "../db";
+import { ResponseToolkit } from "@hapi/hapi";
+import { RowDataPacket } from "mysql2";
+
+export interface RequestParams {
+  identifiers: string[];
+  user_id: number;
+  reporting_month: string;
+}
 
 class PatientService {
-  async getPatientIds() {
-    //TODO
+  async getPatientIds(identifiers: string[]) {
+    let whereQuery = "";
+    identifiers.forEach((identifier) => {
+      whereQuery += `'${identifier}',`;
+    });
+
+    const query = `SELECT DISTINCT patient_id FROM patient_identifier WHERE identifier IN (${whereQuery.slice(
+      0,
+      -1
+    )})`;
+
+    const connection = await AMRS_POOL.getConnection();
+    const result = await connection.execute(query);
+    connection.release();
+
+    return result;
   }
 
-  async queueRDEPatients() {
-    //TODO insert into rde_sync_queue
+  async queueRDEPatients(request: RequestParams, h: ResponseToolkit) {
+    const { identifiers, user_id, reporting_month } = request;
+
+    const [rows] = await this.getPatientIds(identifiers);
+
+    const handleRow = async (row: RowDataPacket) => {
+      const patientId = row.patient_id;
+
+      const moment = require("moment-timezone");
+      let now = moment().tz("Africa/Nairobi");
+      let formattedDateTime = now.format("YYYY-MM-DD HH:mm:ss");
+
+      let query = `INSERT INTO rde_sync_queue (user_id, patient_id, date_created, reporting_month, status) VALUES (${user_id}, ${patientId}, '${formattedDateTime}', '${reporting_month}', 'QUEUED')`;
+
+      const connection = await ETL_POOL.getConnection();
+      const [rows] = await connection.execute(query);
+      connection.release();
+      h.response(rows).created;
+    };
+
+    if (Array.isArray(rows)) {
+      rows.forEach((row: any) => {
+        return handleRow(row as RowDataPacket);
+      });
+    }
   }
 }
 
